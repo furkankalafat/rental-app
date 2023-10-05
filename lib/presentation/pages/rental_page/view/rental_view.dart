b1c09c3b472341f1c7e79317aa9d9d50cb69df1b
Furkan KALAFAT <>
2023-10-05 07:09:30 +0300

    Rental View edited.

--- lib/presentation/pages/rental_page/view/rental_view.dart
+++ lib/presentation/pages/rental_page/view/rental_view.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class HomeView extends StatelessWidget {
-  const HomeView({super.key});
+class RentalView extends StatelessWidget {
+  const RentalView({super.key});
 
   @override
   Widget build(BuildContext context) {
